9a46c83e416e5b7ed4bb431bed7de104f2879ae8
Justin W <48862763+xplayer9@users.noreply.github.com>
2022-07-15 20:41:34 +0800

    add private

--- src/com/git/Spring.java
+++ src/com/git/Spring.java
@@ -1,10 +1,11 @@
 package com.git;
 
 public class Spring {
-    String title;
-    String note="nnn";
-    int count=0;
+    private String title;
+    private String note="nnn";
+    private int count=0;
     public Spring(String s) {
         title = s;
     }
+
 }
